8d2f97ac7eab91ecd99edcb0b7ffef4230b72786
BBorbuev <beksultan.borbuev@zensoft.io>
2019-06-27 12:20:07 +0600

    [*] Updated tests

--- server/src/test/java/kg/apps/endpoint/impl/DefaultWalletEndpointTest.java
+++ server/src/test/java/kg/apps/endpoint/impl/DefaultWalletEndpointTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,41 +14,45 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
-
+@Sql(value = "classpath:scenario_1.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @SpringBootTest
 class DefaultWalletEndpointTest {
+
+    private final static String INSUFFICIENT_FUNDS_MESSAGE = "Insufficient funds";
+    private final static String OK_MESSAGE = "Ok";
 
     @Autowired
     private WalletEndpoint walletEndpoint;
 
     @Test
-    void checkWalletEndpoint_givenUserWithEmptyBalance_shouldResponseForAllAsserts() {
+    void checkWalletEndpoint_scenario_1() {
+
         Wallet.Response response = withdrawalWith200Usd();
-        assertThat(response.getMessage()).isEqualTo("Insufficient funds");
+        assertThat(response.getMessage()).isEqualTo(INSUFFICIENT_FUNDS_MESSAGE);
 
         List<Wallet.Balance> balancesAfterFirstDeposit = depositWith100UsdAndReturnBalance();
         assertUsdBalanceForUserOne(balancesAfterFirstDeposit, 100);
 
         Wallet.Response responseAfterSecondWithdraw = withdrawalWith200Usd();
-        assertThat(responseAfterSecondWithdraw.getMessage()).isEqualTo("Insufficient funds");
+        assertThat(responseAfterSecondWithdraw.getMessage()).isEqualTo(INSUFFICIENT_FUNDS_MESSAGE);
 
         List<Wallet.Balance> balanceAfterSecondDeposit = depositWith100UsdAndReturnBalance();
         assertUsdBalanceForUserOne(balanceAfterSecondDeposit, 200);
 
         Wallet.Response responseAfterThirdWithdrawal = withdrawalWith200Usd();
-        assertThat(responseAfterThirdWithdrawal.getMessage()).isEqualTo("Insufficient funds");
+        assertThat(responseAfterThirdWithdrawal.getMessage()).isEqualTo(INSUFFICIENT_FUNDS_MESSAGE);
 
         List<Wallet.Balance> responseAfterThirdDeposit = depositWith100UsdAndReturnBalance();
         assertUsdBalanceForUserOne(responseAfterThirdDeposit, 300);
 
         Wallet.Response responseAfterFourthWithdrawal = withdrawalWith200Usd();
-        assertThat(responseAfterFourthWithdrawal.getMessage()).isEqualTo("Ok");
+        assertThat(responseAfterFourthWithdrawal.getMessage()).isEqualTo(OK_MESSAGE);
 
         List<Wallet.Balance> balanceAfterFourthWithdrawal = balanceForUserOne();
         assertUsdBalanceForUserOne(balanceAfterFourthWithdrawal, 100);
 
         Wallet.Response responseAfterFifthWithdrawal = withdrawalWith200Usd();
-        assertThat(responseAfterFifthWithdrawal.getMessage()).isEqualTo("Insufficient funds");
+        assertThat(responseAfterFifthWithdrawal.getMessage()).isEqualTo(INSUFFICIENT_FUNDS_MESSAGE);
     }
 
     private List<Wallet.Balance> depositWith100UsdAndReturnBalance() {
